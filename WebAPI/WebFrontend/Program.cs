153e709b8626e40122919abc97974eab4387db38
Fictional <fictionalaroma@gmail.com>
2023-04-08 11:38:53 +0100

    Build Issue for Main, sorted database connection

--- WebAPI/WebFrontend/Program.cs
+++ WebAPI/WebFrontend/Program.cs
@@ -1,4 +1,5 @@
 using AspNet.Security.OAuth.Twitch;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.Web;
@@ -16,29 +17,7 @@ namespace WebFrontend
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
-            var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
-            builder.Services.AddDbContext<ApplicationDbContext>(options => {
-                options.UseSqlite(connectionString);
-            });
-            builder.Services.AddDatabaseDeveloperPageExceptionFilter();
-
-            builder.Services.AddIdentity<IdentityUser, IdentityRole>()
-                .AddEntityFrameworkStores<ApplicationDbContext>()
-                .AddDefaultUI();
-            //builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
-
-            //    .AddEntityFrameworkStores<ApplicationDbContext>();
-
-            builder.Services.AddAuthentication()
-                .AddTwitch(options =>
-                {
-                    options.
-                })
-            builder.Services.AddRazorPages();
-            builder.Services.AddServerSideBlazor();
-            builder.Services.AddScoped<AuthenticationStateProvider, RevalidatingIdentityAuthenticationStateProvider<IdentityUser>>();
-            builder.Services.AddSingleton<WeatherForecastService>();
+           builder = ConfigureServices(builder);
 
             var app = builder.Build();
 
@@ -68,6 +47,47 @@ namespace WebFrontend
             app.MapFallbackToPage("/_Host");
 
             app.Run();
+        }
+
+        private static WebApplicationBuilder ConfigureServices(WebApplicationBuilder builder)
+        {
+            // Add services to the container.
+            var connectionString = builder.Configuration.GetConnectionString("LocalTest") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+            builder.Services.AddDbContext<ApplicationDbContext>(options =>
+            {
+                options.UseSqlite(connectionString);
+            });
+            builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+            builder.Services.AddIdentity<IdentityUser, IdentityRole>()
+                .AddEntityFrameworkStores<ApplicationDbContext>()
+                .AddDefaultUI();
+            //builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+
+            //    .AddEntityFrameworkStores<ApplicationDbContext>();
+
+            var twtichConfig = builder.Configuration.GetSection("TwitchAuth") ?? throw new InvalidOperationException("Twitch Auth not found");
+
+            builder.Services.AddAuthentication(options =>
+            {
+                options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+            })
+            .AddCookie(options =>
+            {
+                options.LoginPath = "/signin";
+                options.LogoutPath = "/signout";
+            })
+            .AddTwitch(options =>
+            {
+                options.ClientId = twtichConfig["ClientID"];
+                options.ClientSecret = twtichConfig["ClientSecret"];
+            });
+            builder.Services.AddRazorPages();
+            builder.Services.AddServerSideBlazor();
+            builder.Services.AddScoped<AuthenticationStateProvider, RevalidatingIdentityAuthenticationStateProvider<IdentityUser>>();
+            builder.Services.AddSingleton<WeatherForecastService>();
+
+            return builder;
         }
     }
 }
