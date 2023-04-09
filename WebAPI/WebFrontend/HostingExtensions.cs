e8ea12302575c9817abe99549bf0b2eb878ba8f9
Fictional <fictionalaroma@gmail.com>
2023-04-09 09:47:19 +0100

    Refactor

--- WebAPI/WebFrontend/HostingExtensions.cs
+++ WebAPI/WebFrontend/HostingExtensions.cs
@@ -51,5 +51,35 @@ namespace WebFrontend
             return builder;
 
         }
+
+        public static WebApplication ConfigurePipeline(this WebApplication app)
+        {
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseMigrationsEndPoint();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.MapControllers();
+            app.MapBlazorHub();
+            app.MapFallbackToPage("/_Host");
+
+            return app;
+        }
     }
 }
--- WebAPI/WebFrontend/Program.cs
+++ WebAPI/WebFrontend/Program.cs
@@ -17,31 +17,8 @@ namespace WebFrontend
             builder.ConfigureServices();
 
             var app = builder.Build();
+            app.ConfigurePipeline();
 
-            // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseMigrationsEndPoint();
-            }
-            else
-            {
-                app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
-
-            app.UseHttpsRedirection();
-
-            app.UseStaticFiles();
-
-            app.UseRouting();
-
-            app.UseAuthentication();
-            app.UseAuthorization();
-
-            app.MapControllers();
-            app.MapBlazorHub();
-            app.MapFallbackToPage("/_Host");
 
             app.Run();
         }
