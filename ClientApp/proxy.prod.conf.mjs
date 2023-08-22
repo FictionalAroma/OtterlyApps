66aed53368320cf9affb62f48802026503a14b99
Lucy Daniels <lucy.daniels.software@gmail.com>
2023-08-22 17:07:26 +0100

    update proxy

--- ClientApp/proxy.prod.conf.mjs
+++ ClientApp/proxy.prod.conf.mjs
@@ -3,7 +3,7 @@ export default [
     context: [
         '/bff'
         ],
-    target: 'https://localhost:7161',
+    target: 'https://ec2-3-249-25-187.eu-west-1.compute.amazonaws.com:7172',
     secure: false,
     headers: {
       Connection: 'Keep-Alive'
