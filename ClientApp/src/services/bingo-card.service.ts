17e06cc4c09326302e4635ad9bc12feea138df41
Lucy Daniels <lucy.daniels.software@gmail.com>
2023-08-23 14:40:04 +0100

    add base URL environment

--- ClientApp/src/services/bingo-card.service.ts
+++ ClientApp/src/services/bingo-card.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 import { BaseResponse, BingoCardDTO, BingoSessionDTO } from 'api/otterlyapi';
 import { HttpHeaders } from '@angular/common/http';
 import { BingoCardDTOImp } from 'api/bingoApiImp';
+import { environment } from 'src/environments/environment';
 const httpOptions = {
   headers: new HttpHeaders({'Content-Type':'application/json',}),
   body: ""
@@ -13,26 +14,27 @@ const httpOptions = {
 })
 export class BingoCardService {
   constructor(private http: HttpClient) {
+
   }
 
   getCardsObservable()
   {
-    return this.http.get<BingoCardDTO[]>("bff/bingo/getcards")
+    return this.http.get<BingoCardDTO[]>(`${environment.apiUrl}bff/bingo/getcards`)
   }
 
   updateCard(cardUpdate : BingoCardDTO)
   {
-    this.http.post("bff/bingo/UpdateCard", JSON.stringify(cardUpdate), httpOptions).subscribe();
+    this.http.post(`${environment.apiUrl}bff/bingo/UpdateCard`, JSON.stringify(cardUpdate), httpOptions).subscribe();
   }
 
   addCard(newCard: BingoCardDTOImp) {
-    return this.http.put<BingoCardDTOImp>("bff/bingo/AddCard",JSON.stringify(newCard), httpOptions )
+    return this.http.put<BingoCardDTOImp>(`${environment.apiUrl}bff/bingo/AddCard`,JSON.stringify(newCard), httpOptions )
   }
   delete(newCard: BingoCardDTO) {
 
     let deleteOptions = {...httpOptions};
     deleteOptions.body = JSON.stringify(newCard);
-    return this.http.delete<boolean>("bff/bingo/DeleteCard",deleteOptions)
+    return this.http.delete<boolean>(`${environment.apiUrl}bff/bingo/DeleteCard`,deleteOptions)
   }
 
 
